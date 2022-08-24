5b9d6091abaaec20dd8927366caa8959be813ec4
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-08-24 19:50:44 +0200

    all functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 // react imports
 import { useState } from "react";
 
+// libraries
+import JSConfetti from "js-confetti";
+const jsConfetti = new JSConfetti();
+
 // custom components
 import CustomForm from "./components/CustomForm";
 import OneThing from "./components/OneThing";
@@ -32,14 +36,18 @@ function App() {
     setThing(event.target.value);
   };
 
-  const handleCompletedThing = (event) => {
+  const handleCompletedThing = async (event) => {
     event.target.setAttribute("disabled", true);
     setThing(getSuccessMessage(event.target.value));
+    await jsConfetti.addConfetti();
+    event.target.removeAttribute("disabled");
+    setThing("");
+    setIsCompleted(true);
   };
 
   return (
-    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200">
-      <div className="grid place-items-center gap-8 m-8">
+    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200 ">
+      <div className="grid place-items-center gap-8 m-8 ">
         {isCompleted && (
           <CustomForm
             thing={thing}
