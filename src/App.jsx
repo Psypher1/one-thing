213376b2e975756ca8aae6b33525fab91bdab063
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-08-24 19:52:44 +0200

    refactored isCompleted

--- src/App.jsx
+++ src/App.jsx
@@ -48,14 +48,13 @@ function App() {
   return (
     <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200 ">
       <div className="grid place-items-center gap-8 m-8 ">
-        {isCompleted && (
+        {isCompleted ? (
           <CustomForm
             thing={thing}
             handleInput={handleInput}
             handleSubmit={handleSubmit}
           />
-        )}
-        {!isCompleted && (
+        ) : (
           <OneThing thing={thing} handleCompletedThing={handleCompletedThing} />
         )}
       </div>
